90fe66313699cbc7c623ffa23e8bb608de9dcfa2
rektide <rektide@voodoowarez.com>
2010-02-02 02:16:21 -0700

    Use new Chain deferred code for fs. Dont send headers until body is ready and can send Content-Length.

--- fs.js
+++ fs.js
@@ -9,28 +9,23 @@ var FileSystemFilter = function(basePath)
 		var url = ctx.request.url;
 		var pth = path.join(basePath,url);
 		
-		posix.open(pth,process.O_RDONLY,0)
-		  .addCallback(function(fd){
-			var response = ctx.response;
-			response.sendHeader(200,{});
+		posix.open(pth,process.O_RDONLY,0).addCallback(function(fd){
 			posix.read(fd,readLength,0).addCallback(function(data,len){
 				var response = ctx.response;
+				response.sendHeader(200,{"Content-Length": data.length});
 				response.sendBody(data);
 				response.finish();
 				
-				Chain.resume(ctx);
+				ctx.chain.chainResult.emit("success",ctx,true);
 			});
-		})
-                  .addErrback(function(){
+		}).addErrback(function(){
 			var response = ctx.response;
 			response.sendHeader(404,{});
 			response.finish();
-
-			Chain.resume(ctx);
+			
+			ctx.chain.chainResult.emit("success",ctx,true);
 		});
 
 		return "defer";
 	}
-	
-	
 };
