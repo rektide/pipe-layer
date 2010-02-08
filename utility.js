9c513a7fff36786626f2d8975ca3cff22ad94c54
rektide <rektide@voodoowarez.com>
2010-02-08 10:51:17 -0700

    Add utility.coroutine and helper function utility.bind.

--- utility.js
+++ utility.js
@@ -1,5 +1,5 @@
 // based on http://jsfromhell.com/array/search with sort added.
-var search = function(o, v, s, i){
+var binarySearch = function(o, v, s, i){
 	
 	if(!s) s= function(a,b){return a-b;};
 	var h = o.length, l = -1, m;
@@ -16,3 +16,37 @@ var orderedInsert = function(target,element,sort) {
 	target.splice(i,0,element);
 	return i;
 }
+
+var bind = function(f,ctx) {
+
+	return new function(g,ctxb) {
+		return function()
+		{
+			return g.apply(ctxb,arguments);
+		}	
+	}(f,ctx);
+}
+
+var coroutine = function()
+{
+	this.final = arguments.length;
+	this.count = 0;
+
+	this.join = function()
+	{
+		sys.debug("join "+sys.inspect(this));
+		if(++this.count==this.final) this.emitSuccess();
+	}
+
+	for(var i in arguments)
+	{
+		sys.debug("arg "+i);
+		if(arguments[i] instanceof events.Promise)
+		{
+			sys.debug("add callback");
+			arguments[i].addCallback( bind(this.join,this) );
+		}
+	}
+	
+}
+coroutine.prototype = new events.Promise();
