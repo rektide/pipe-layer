5b81ee9c35f6177c7e8ff13f0ea4c5f4bbce9d88
rektide <rektide@voodoowarez.com>
2010-02-25 04:16:01 -0700

    Bring pXHR closer to spec: readyState ordering, const definitions, and chiefly, no separate response object.

--- src/main/html/pipe.xhr.js
+++ src/main/html/pipe.xhr.js
@@ -2,46 +2,68 @@ var worker_file = 'worker.xhr.js',
 	worker_name = 'xpipe'
 
 var XMLHttpRequest = function() {
-	
-	this.worker = new SharedWorker(worker_file,worker_name)
 
-	this.worker.onerror = function(e) {
+	// states
+	this.UNSENT = 0
+	this.OPENED = 1
+	this.HEADERS_RECEIVED = 2
+	this.LOADING = 3
+	this.DONE = 4
+	
+	this.readyState = 0
+
+	// response properties
+	this.status = null
+	this.statusText = null
+	this.responseText = null
+	this.responseXML = null
+
+	// internal fields
+	this._req = {}
+	
+	this._worker = new SharedWorker(worker_file,worker_name)
+	this._worker.onerror = function(e) {
 		console.log("xhr worker-error "+e)
 	}
-
+	
 	this.open = function(method,url,async,user,password) {
 	
-		console.log("xhr open")	
 		if(async == false)
 			throw "XH async only"
-		this.method = method
-		this.url = url
+		console.log("xhr open")	
+		
+		var req = this._req
+		req.method = method
+		req.url = url
 		if(user)
-			this.user = user
+			req.user = user
 		if(password)
-			this.password = password
-		this.headers = {}
+			req.password = password
+		req.headers = {}
+		
+		this.readyState = 1
 	}
 	
 	this.setRequestHeader = function(header,value) {
 		
-		console.log("xhr set-request-header")	
-		this.headers[header] = value
+		if(this.readyState != 1)
+			throw "INVALID_STATE_ERR"
+		console.log("xhr set-request-header")
+		
+		this._req.headers[header] = value
 	}
 	
 	this.send = function(data) {
-		
+	
+		if(this.readyState != 1)
+			throw "INVALID_STATE_ERR"
 		console.log("xhr send")
-		if(data !== undefined)
-			this.req["data"] = data
-
-		var resp = this.resp = {readyState:0, responseText:""}
-		this.worker.port._resp = resp
-		this.worker.port._ready = this.onreadystatechange
-
-		this.worker.port.postMessage(this.req)
 		
-		return resp
+		if(data !== undefined)
+			this._req["data"] = data
+
+		this._worker.port._xhr = this
+		this._worker.port.postMessage(this._req)
 	}
 	
 	this.abort = function() {
@@ -49,41 +71,41 @@ var XMLHttpRequest = function() {
 	}
 
 	// bind a listener
-	this.worker.port.addEventListener('message',function(e){
+	this._worker.port.addEventListener('message',function(e){
 	
 		debugger
 	
 		console.log("xhr got-back")	
 		var msg = e.data
-		var resp = this._resp
+		var xhr = this._xhr
 		
-		resp.readyState = msg.readyState
+		xhr.readyState = msg.readyState
 	
-		var headers	= msg["headers"]
+		var headers = msg["headers"]
 		if(headers !== undefined)
-			resp.headers = headers
+			xhr.headers = headers
 		var status = msg["status"]
 		if(status !== undefined)
-			resp.status = status
+			xhr.status = status
 		var statusText = msg["statusText"]
 		if(statusText !== undefined)
-			resp.statusText = statusText
+			xhr.statusText = statusText
 		
-		resp.pipe = msg.pipe
-		resp.seq = msg.seq
-		resp.rseq = msg.rseq
-
-		resp.responseText = this.responseText + msg.responseDelta 
+		xhr.pipe = msg.pipe
+		xhr.seq = msg.seq
+		xhr.rseq = msg.rseq
+		
+		xhr.responseText = xhr.responseText || "" + msg.responseDelta 
 
 		var responseXML = this["responseXML"]
 		if(responseXML !== undefined)
-			resp.responseXML = responseXML
+			xhr.responseXML = responseXML
 
-		var ready = this._ready
+		var ready = xhr.onreadystatechange
 		if(ready !== undefined)
-			ready.call(resp)
+			ready.call(xhr)
 		
 	}, false)
-	
-	this.worker.port.start()
+
+	this._worker.port.start()
 }
