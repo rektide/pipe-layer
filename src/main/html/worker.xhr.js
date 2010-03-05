95994d831f5471b20492a87cde6a078512c410da
rektide <rektide@voodoowarez.com>
2010-03-04 22:39:06 -0700

    Worker port/pipe queue.

--- src/main/html/worker.xhr.js
+++ src/main/html/worker.xhr.js
@@ -12,10 +12,17 @@ var clientDb = {}
 
 onconnect = function(e) {
 
+	// capture port
 	var port = e.ports[0]
 	port.onmessage = onmessage
-	
-	var pipe = port._pipe = uuid()
+
+	// port is a new pipe; build pipe.
+	var pipe = uuid()
+	port._pipe = pipe // pipe id
+	port._rseq = 1
+	port._seq = 1
+	port._seqTicket = 1
+	port._deferred = []
 	clientDb[pipe] = port
 }
 
@@ -26,9 +33,18 @@ var onmessage = function(e) {
 
 	var xhr = new XMLHttpRequest()
 	xhr.open(req.method, req.url, true, req["user"], req["password"])
+
+	// set imported headers
 	for(var h in req.headers)
 		xhr.setRequestHeader(h, req.headers[h])
+	// add x-pipe and x-seq headers 
+	xhr.setRequestHeader("X-Pipe", port._pipe)
+	xhr.setRequestHeader("X-Seq", port._seqTicket++)
+
+	// used only if server doesnt end up supporting x-pipe
+	// otherwise, we look up the clientDb from the x-pipe
 	xhr._port = port
+	
 	xhr.onreadystatechange = xhrHandler
 	xhr.send(req["data"])
 }
@@ -39,9 +55,46 @@ var xhrHandler = function(e,h) {
 	// require at least headers to be done
 	if(this.readyState < 2)
 		return
-
+	
 	// prepare a response
-	var msg = {readyState: this.readyState}
+	var msg = this["_message"]
+	var continuation = msg === undefined // are we the continuation of another deferred message?
+	if(msg == undefined)
+		msg = {}
+	msg.readyState = this.readyState // 3 may be around for a while
+
+	// lookup pipe
+	var pipe = this._pipe
+	if(pipe === undefined) {
+	
+		pipe = headers["X-Pipe"] || headers["x-pipe"]
+		if(pipe === undefined) return
+
+		msg.pipe = pipe
+	}
+	if(pipe === undefined) {
+		
+		// ok this is a really weird context-- the server is asking to create its own pipe to us.
+		pipe = headers["X-Create-Pipe"] || headers["x-create-pipe"]
+		if(pipe === undefined) return
+
+		msg.pipe = pipe
+		
+		// need to set up a router & global event dispatcher to figure out who (which pages) gets to talk to the new pipe
+	
+		// create a clientDb[pipe] entry to handle
+		// this ought be another rhttp gateway
+	}
+	// non- pipelined server, reply is to original request
+	if(pipe === undefined) {
+
+		pipe = this._port._pipe
+	}
+	this._pipe = pipe
+
+	// lookup port
+	var port = clientDb[pipe]
+	
 
 	// initial XHR handle call
 	if(this.readyState == 2 || this["_headersSent"] == undefined) {
@@ -68,54 +121,63 @@ var xhrHandler = function(e,h) {
 			// write complete header line to headers
 			headers[headerKey] = headerValue
 		}
-		
+	
 		// install headers		
 		msg.headers = headers
-		
-		// load sequence
-		this._seq = headers["X-Seq"]
-		this._rseq = headers["X-RSeq"]
 	
-		var pipe = headers["X-Create-Pipe-Id"]
-		if(pipe !== undefined) {
-			this._pipe = pipe
-		}
-		else
-			this._pipe = headers["X-Pipe-Id"]
-		
-		// non- pipelined server, reply is to original request
-		if(this._pipe === undefined)
-			this._pipe = this._port._pipe
-		
 		// load status
 		msg.status = this.status
 		msg.statusText = this.statusText
-		
+	
+		// load sequence
+		msg.seq = headers["X-Seq"] || headers["x-seq"]
+		msg.rseq = headers["X-RSeq"] || headers["x-rseq"]
+	
 		// headers are queued to send
 		this._headersSent = true
 	}
 	
-	// address outgoing message
-	msg.pipe = this._pipe
-	msg.seq = this._seq
-	msg.rseq = this._rseq
-	
 	// read in body content
-	var bodyLen = this["bodyLen"] || 0
+	var bodyLen = this["_bodyLen"] || 0
+	
 	msg.responseDelta = this.responseText.substr(bodyLen)
-	this.bodyLen = this.responseText.length
+	this._bodyLen = this.responseText.length
 	
 	// read in responseXML
 	if(this.readyState == 4) {
+
 		try {
 			msg.responseXML = this.responseXML
 		} catch (err) {}
 	}
 	
-	// lookup port
-	var port = clientDb[this._pipe]
-	// send	
-	port.postMessage(msg)
+	// check whether we should defer
+	if(msg.seq == port._seq) {
+
+		// we are the current -- dont defer, send
+		port.postMessage(msg)
+
+		// check for deferred to send
+		while(port._deferred.length && port._deferred[0].seq == port._seq) {
+
+			// send the next message
+			var msg = port._deferred.shift()
+			postMessage.send(msg)
+
+			// advance if the message was at the end
+			if(msg.readyState == 4) {
+				
+				++port._seq
+			}
+			
+		}
+	}
+	else if(!continuation) {
+			
+		// havent already queued/deferred a message, and we're further down the queue
+		orderedInsert(port._deferred, msg, function(a,b) { return a.seq - b.seq })
+		// since we're deferring, build any future xhr state upon this existing message
+		this._message = msg
+	} 
+	// otherwise, we've been building a message and its queued to send.  we've updated state, so we're done.
 }
-
-
