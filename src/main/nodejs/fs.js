b903c6a7e937f1ef25a03cb50e4218c3c72ff0a0
rektide <rektide@voodoowarez.com>
2010-03-09 01:06:42 -0700

    Migrate from old posix impl of FileSystemFilter to node's new fs. Rely on BaseFilter for success/failure cases.

--- src/main/nodejs/fs.js
+++ src/main/nodejs/fs.js
@@ -9,23 +9,31 @@ var FileSystemFilter = function(basePath)
 		var url = ctx.request.url
 		var pth = path.join(basePath,url)
 		
-		posix.open(pth,process.O_RDONLY,0).addCallback(function(fd){
-			posix.read(fd,readLength,0).addCallback(function(data,len){
+		fs.open(pth,process.O_RDONLY,0,function(err,fd) {
+			
+			if(err) {
+				this.failure(ctx,null,404)
+				return
+			}
+
+			fs.read(fd,readLength,0, function(err,data,len){
+
+				if(err) {
+					this.failure(ctx,null,500)
+					return
+				}
+
 				var response = ctx.response
 				response.sendHeader(200,{"Content-Length": data.length})
 				response.sendBody(data)
 				response.finish()
-				
-				ctx.chain.chainResult.emit("success",ctx,true)
-			})
-		}).addErrback(function(){
-			var response = ctx.response
-			response.sendHeader(404,{})
-			response.finish()
 			
-			ctx.chain.chainResult.emit("success",ctx,true)
-		})
-
+				this.success(ctx)	
+			})
+		}
+		
 		return "defer"
 	}
 }
+
+inherit.inherit(FileSystemFilter,DefaultBaseFilter)
